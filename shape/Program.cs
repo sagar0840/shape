c5a881279ed37842e43ce20c05d77714149cd8df
sagar0840 <sagarkale55556@gmail.com>
2023-01-25 16:25:41 +0530

    Add project files.

---
+++ shape/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace shape
+{ 
+    public static class Program
+{
+
+    static void Main(string[] args)
+    {
+        shape.Circle c1 = new shape.Circle(5);
+        c1.CalculateArea();
+        Console.WriteLine(c1);
+    }
+}
+}
---
+++ shape/shape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    abstract internal class shape
+    {
+        public abstract void CalculateArea();
+
+        public class Circle : shape
+        {
+            private int radius;
+            private double area;
+            public Circle(int radius)
+            {
+                this.radius = radius;
+            }
+            public override void CalculateArea()
+            {
+                area = 3.14 * radius * radius;
+            }
+            public override string ToString()
+            {
+                return $"Area of Circle is {area}";
+            }
+        }
+    }
+}
+
+
